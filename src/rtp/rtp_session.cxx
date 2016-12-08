45c0013d929f99633448da6976cb5453689742e5
Robert Jongbloed <robert.jongbloed@blackboard.com>
2016-12-08 17:44:42 +0000

    Fixed merge error

--- src/rtp/rtp_session.cxx
+++ src/rtp/rtp_session.cxx
@@ -1018,8 +1018,6 @@ RTPHeaderExtensions OpalRTPSession::GetHeaderExtensions() const
 }
 
 
-const PString & OpalRTPSession::GetAbsSendTimeHdrExtURI() { static const PConstString s("http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time"); return s; }
-
 void OpalRTPSession::SetHeaderExtensions(const RTPHeaderExtensions & ext)
 {
   PSafeLockReadWrite lock(*this);
