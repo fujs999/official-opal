a1de7b18398bb3a61438841aa068e6c88e47c2f8
Robert Jongbloed <robertj@voxlucida.com.au>
2020-06-12 15:03:39 +0100

    Added festival TTS development libraries to bootstrap build script.

--- samples/server/build_server.sh
+++ samples/server/build_server.sh
@@ -70,7 +70,7 @@ if $BOOTSTRAP; then
     		libncurses-dev libsdl2-dev libavformat-dev libswscale-dev \
     		libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgsm1-dev \
     		libspeex-dev libopus-dev libavcodec-dev libx264-dev libvpx-dev \
-    		libtheora-dev libspandsp-dev capiutils dahdi
+    		libtheora-dev libspandsp-dev capiutils dahdi festival-dev
     elif which yum > /dev/null 2> /dev/null; then
         sudo yum-config-manager --enable epel
     	sudo yum install \
@@ -79,7 +79,7 @@ if $BOOTSTRAP; then
     		ncurses-devel SDL2-devel libavformat-devel libswscale-devel \
     		gstreamer1.0-devel gstreamer-plugins-base1.0-devel gsm-devel \
     		speex-devel opus-devel avcodec-devel x264-devel libvpx-devel \
-    		theora-devel libspandsp-devel capiutils dahdi
+    		theora-devel libspandsp-devel capiutils dahdi festival-devel
     else
     	echo "What OS is this? No apt or yum!"
     	exit 1
