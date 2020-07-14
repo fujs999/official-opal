d162caa009304620cdda220ef6b5ecf5393c18b9
Gavin Llewellyn <gavin.llewellyn@blackboard.com>
2020-07-14 10:19:22 +0100

    Revert incomplete protection of OpalMediaSession::m_offeredCryptokeys
    A single thread should call OfferCryptoSuite/GetOfferedCryptoKeys
    
    JIRA: WEBRTC-4510

--- src/opal/mediasession.cxx
+++ src/opal/mediasession.cxx
@@ -1721,15 +1721,12 @@ void OpalMediaSession::OfferCryptoSuite(const PString & cryptoSuiteName)
 
   OpalMediaCryptoKeyInfo * keyInfo = cryptoSuite->CreateKeyInfo();
   keyInfo->Randomise();
-
-  P_INSTRUMENTED_LOCK_READ_WRITE(return);
   m_offeredCryptokeys.Append(keyInfo);
 }
 
 
 OpalMediaCryptoKeyList & OpalMediaSession::GetOfferedCryptoKeys()
 {
-  PSafeLockReadOnly lock(*this);
   return m_offeredCryptokeys;
 }
 
