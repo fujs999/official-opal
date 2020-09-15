c8df3b1691aba0a9a5efc6e38006acf140dca9dc
Robert Jongbloed <robertj@voxlucida.com.au>
2020-09-15 11:13:04 +0100

    Forgot to update script for installing OpenH264 binary, when updated the header files.

--- plugins/install_openh264.sh
+++ plugins/install_openh264.sh
@@ -11,22 +11,18 @@ case `uname -sm` in
    Linux*x86_64 )
      PLATFORM=linux64
      EXT=so
-     LINKEXT=so.4
    ;;
    Linux*x86 )
      PLATFORM=linux32
      EXT=so
-     LINKEXT=so.4
    ;;
    Darwin*x86_64 )
      PLATFORM=osx64
      EXT=dylib
-     LINKEXT=4.dylib
    ;;
    Darwin*x86 )
      PLATFORM=osx32
      EXT=dylib
-     LINKEXT=4.dylib
    ;;
    * )
      echo Unknown platform `uname -sm`
@@ -35,7 +31,7 @@ case `uname -sm` in
 esac
 
 LIBBASE=libopenh264
-LIBFILE=${LIBBASE}-1.8.0-${PLATFORM}.4.$EXT
+LIBFILE=${LIBBASE}-2.1.0-${PLATFORM}.5.$EXT
 LIBDIR=${1-/usr/local/lib}
 
 if [ \! -d $LIBDIR ]; then
@@ -45,7 +41,6 @@ fi
 cd $LIBDIR
 rm -f ${LIBBASE}*
 curl http://ciscobinary.openh264.org/$LIBFILE.bz2 | bunzip2 > $LIBFILE
-ln -s $LIBFILE ${LIBBASE}.$LINKEXT
 ln -s $LIBFILE ${LIBBASE}.$EXT
 ls -l $LIBDIR/${LIBBASE}*
 
