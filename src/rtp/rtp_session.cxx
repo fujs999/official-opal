c4eae7ec1d8dca25f0a8a5901c31d7adc8a05942
Robert Jongbloed <robert.jongbloed@blackboard.com>
2021-03-25 16:51:45 +0000

    Prevent log spam caused by Chrome bandwidth "probe"

--- src/rtp/rtp_session.cxx
+++ src/rtp/rtp_session.cxx
@@ -1171,7 +1171,8 @@ OpalRTPSession::SendReceiveStatus OpalRTPSession::SyncSource::OnReceiveRetransmi
 {
   PINDEX payloadSize = frame.GetPayloadSize();
   if (payloadSize < 2) {
-    PTRACE(2, &m_session, *this << "retransmission packet too small: " << frame);
+    // Padded zero payload packets can be used as bandwidth probe
+    PTRACE_IF(2, payloadSize > 0, &m_session, *this << "retransmission packet too small: " << frame);
     return e_IgnorePacket;
   }
 
