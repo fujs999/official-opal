b13e6488988d292f73b0f0f18318b3458fee8546
Robert Jongbloed <robertj@voxlucida.com.au>
2021-11-22 18:00:51 +0000

    More tweaks to make it compile everywhere

--- src/opal/pres_ent.cxx
+++ src/opal/pres_ent.cxx
@@ -320,8 +320,11 @@ void OpalPresentity::OnPresenceChange(const OpalPresenceInfo & info)
 {
   PWaitAndSignal mutex(m_notificationMutex);
 
-  if (!m_onPresenceChangeNotifier.IsNULL())
-    m_onPresenceChangeNotifier(*this, PAutoPtr<OpalPresenceInfo>(info.CloneAs<OpalPresenceInfo>()));
+  if (m_onPresenceChangeNotifier.IsNULL())
+    return;
+
+  PAutoPtr<OpalPresenceInfo> pinfo(info.CloneAs<OpalPresenceInfo>());
+  m_onPresenceChangeNotifier(*this, pinfo);
 }
 
 
