78cb8262c36a1815ab3fc9682b790369f2e90133
Robert Jongbloed <robert.jongbloed@blackboard.com>
2022-08-11 17:42:43 +0100

    WASABI-705: Update build
    Allow for move from GitHub
    Correctly use S3 for yum repo
    Matrix build for both el7 and amzn2

--- rpmbuild-docker.sh
+++ rpmbuild-docker.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-PACKAGENAME=bbcollab-libopal
-TARGET_OS=${1:-el7}
-CMD=${2:-./rpmbuild.sh}
+PACKAGENAME=libopal
 
-docker build -f $TARGET_OS.Dockerfile --build-arg SPECFILE=$PACKAGENAME.spec -t $PACKAGENAME-$TARGET_OS-build .
-docker run -it --rm --mount type=bind,src=$PWD,dst=/host -w /host $PACKAGENAME-$TARGET_OS-build $CMD
+docker build --build-arg SPECFILE=$PACKAGENAME.spec -t $PACKAGENAME-build --file el7.Dockerfile .
+docker run -it --rm --mount type=bind,src=$PWD,dst=/work -w /work $PACKAGENAME-build ./rpmbuild.sh "$@"
